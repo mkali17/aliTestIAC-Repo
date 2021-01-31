53a645d4eb02e00f6c8124c1fe884e32492c31ae
mkali7 <mkali7@gmail.com>
2021-01-30 19:24:33 -0500

    ali first IAC pipeline commit

--- iac_pipeline.groovy
+++ iac_pipeline.groovy
@@ -4,7 +4,7 @@ pipeline {
   stages {
     stage('Submit Stack') { 
       steps {
-        //sh "aws cloudformation deploy --template-file  '$workspace/cloudformation/TrainingEvent-UbuntuServer.json' --stack-name TomCatWeb-Stack-Val --region 'us-east-1' --parameter-overrides InstanceType=t2.micro KeyName='DevOpsKeyPair' SSHLocation=0.0.0.0/0"
+        sh "aws cloudformation deploy --template-file  '$workspace/cloudformation/TrainingEvent-UbuntuServer.json' --stack-name aliTomCatWeb-Stack-Val --region 'us-east-1' --parameter-overrides InstanceType=t2.micro KeyName='aliDevOpsKeyPair' SSHLocation=0.0.0.0/0"
         sh "echo SKIPPING INFRASTRUCTURE CREATION/UPDATE for now .."
       }
     }
@@ -23,7 +23,7 @@ pipeline {
       steps {
         catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
           withCredentials([sshUserPrivateKey(credentialsId: '2be93d5b-70f9-4db2-adc3-f307b7b86c48', keyFileVariable: 'myKEY')]) {
-             sh 'ansible-playbook ./ansible/playbooks/tomcat-setup.yml --user ubuntu --key-file ${myKEY}'  
+             //sh 'ansible-playbook ./ansible/playbooks/tomcat-setup.yml --user ubuntu --key-file ${myKEY}'  
            }//end withCredentials
           sh "exit 0"
          }//end catchError
