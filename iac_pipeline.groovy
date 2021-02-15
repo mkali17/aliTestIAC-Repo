389025cd77dca411b6cc78ea6d73e7dddbf941ae
mkali7 <mkali7@gmail.com>
2021-02-14 21:56:25 -0500

    Source Changed in IAC Pipeline - 02-14-2021

--- iac_pipeline.groovy
+++ iac_pipeline.groovy
@@ -6,7 +6,7 @@ pipeline {
       steps {
           catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
             withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'aliTestJenkinsUser02', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-              sh "aws cloudformation deploy --template-file  $workspace/cloudformation/TrainingEvent-UbuntuServer.json --stack-name aliTomCatWeb-Stack-Test --region us-east-1 --parameter-overrides InstanceType=t2.micro KeyName=aliTrainingKeyPair2 SSHLocation=0.0.0.0/0 --tags name=aliTomCatWeb-Stack-Test"
+              sh "aws cloudformation deploy --template-file  $workspace/cloudformation/TrainingEvent-ApacheTomCatServer.json --stack-name aliTomCatWeb-Stack-Test02 --region us-east-1 --parameter-overrides InstanceType=t2.micro KeyName=aliTrainingKeyPair2 SSHLocation=0.0.0.0/0 --tags name=aliTomCatWeb-Stack-Test02"
               //sh "aws cloudformation create-stack --template-body '$workspace/cloudformation/TrainingEvent-UbuntuServer.py' --stack-name TomCatWeb-Stack-Val --region us-east-1 --parameters ParameterKey=InstanceType,ParameterValue=t2.micro ParameterKey=KeyName,ParameterValue='aliTrainingKeyPair2' ParameterKey=SSHLocation,ParameterValue=0.0.0.0/0"
               //sh "echo SKIPPING INFRASTRUCTURE CREATION/UPDATE for now .."
             }//end withCredentials
