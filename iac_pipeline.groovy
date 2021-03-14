5861d30f38b58f675112f6f88a9c24db78a43655
mkali7 <mkali7@gmail.com>
2021-03-14 14:34:00 -0400

    commtted AACPipeline - 03-14-2021

--- iac_pipeline.groovy
+++ iac_pipeline.groovy
@@ -1,14 +1,13 @@
-//Description pipeline
+// Description pipeline
 pipeline {
   agent any
   stages {
     stage('Submit Stack') { 
       steps {
           catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'aliTestJenkinsUser02', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-              sh "aws cloudformation deploy --template-file  $workspace/cloudformation/TrainingEvent-ApacheTomCatServer.json --stack-name aliTomCatWeb-Stack-Test02 --region us-east-1 --parameter-overrides InstanceType=t2.micro KeyName=aliTrainingKeyPair2 SSHLocation=0.0.0.0/0 --tags name=aliTomCatWeb-Stack-Test02"
-              //sh "aws cloudformation create-stack --template-body '$workspace/cloudformation/TrainingEvent-UbuntuServer.py' --stack-name TomCatWeb-Stack-Val --region us-east-1 --parameters ParameterKey=InstanceType,ParameterValue=t2.micro ParameterKey=KeyName,ParameterValue='aliTrainingKeyPair2' ParameterKey=SSHLocation,ParameterValue=0.0.0.0/0"
-              //sh "echo SKIPPING INFRASTRUCTURE CREATION/UPDATE for now .."
+            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'Jenkins-server', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+              //sh "aws cloudformation deploy --template-file $workspace/cloudformation/TrainingEvent-UbuntuServer.json --stack-name TomCatWeb-Stack-Test --region us-east-1 --parameter-overrides InstanceType=t2.micro KeyName=myTestKeyPair02 SSHLocation=0.0.0.0/0 --tags name=TomCatWeb-Stack-Test"
+              sh "echo SKIPPING INFRASTRUCTURE CREATION/UPDATE for now .."
             }//end withCredentials
             sh "exit 0"
          }//end catcherror
@@ -17,10 +16,8 @@ pipeline {
     stage('Update Inventory'){
       steps{
         catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-           //sh "echo SKIPPING Automated update of inventory file in lieu to manual update .."
-             withCredentials([sshUserPrivateKey(credentialsId: '43645869-fe60-4838-ab62-94581a9270d1', keyFileVariable: 'aliTrainingKeyPair2')]) {
-            // sh 'ansible-playbook ./ansible/playbooks/update_inventory.yml --user ubuntu --key-file ${myTestKeyPair02}'  
-            //  sh '/etc/ansible/ec2.py --list'
+              withCredentials([sshUserPrivateKey(credentialsId: 'a59a13e3-8e2f-4920-83c9-a49b576e5d58', keyFileVariable: 'myTestKeyPair02')]) {
+                sh 'ansible-playbook ./ansible/playbooks/update_inventory.yml --user ubuntu --key-file ${myTestKeyPair02}' 
            }//end withCredentials
           sh "exit 0"
          }//end catchError
@@ -29,9 +26,9 @@ pipeline {
     stage('Configure Tomcat') {
       steps {
         catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-          withCredentials([sshUserPrivateKey(credentialsId: 'a59a13e3-8e2f-4920-83c9-a49b576e5d58', keyFileVariable: 'myKEY')]) {
-         //    sh 'ansible-playbook ./ansible/playbooks/tomcat-setup.yml --user ubuntu --key-file ${myKEY}'  
-           }//end withCredentials
+          withCredentials([sshUserPrivateKey(credentialsId: 'a59a13e3-8e2f-4920-83c9-a49b576e5d58', keyFileVariable: 'myTestKeyPair02')]) {
+            sh 'ansible-playbook ./ansible/playbooks/tomcat-setup.yml --user ubuntu -vvv --key-file ${myTestKeyPair02}'
+            }//end withCredentials
           sh "exit 0"
          }//end catchError
       }//end steps
