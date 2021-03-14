fae57644d8504caf3a94aaf3f0e749d8f3defdfc
mkali7 <mkali7@gmail.com>
2021-03-14 15:22:16 -0400

    commtted IACPipeline - 03-14-2021-2

--- iac_pipeline.groovy
+++ iac_pipeline.groovy
@@ -5,7 +5,7 @@ pipeline {
     stage('Submit Stack') { 
       steps {
           catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'aliTestJenkinsUser01', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'aliTestJenkinsUser02', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY']]) {
               //sh "aws cloudformation deploy --template-file $workspace/cloudformation/TrainingEvent-UbuntuServer.json --stack-name TomCatWeb-Stack-Test --region us-east-1 --parameter-overrides InstanceType=t2.micro KeyName=myTestKeyPair02 SSHLocation=0.0.0.0/0 --tags name=TomCatWeb-Stack-Test"
               sh "echo SKIPPING INFRASTRUCTURE CREATION/UPDATE for now .."
             }//end withCredentials
