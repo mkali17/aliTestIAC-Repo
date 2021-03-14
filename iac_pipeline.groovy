bb13cbf7a7f267b53b8563f8c6f0bcd6852e143f
mkali7 <mkali7@gmail.com>
2021-03-14 15:49:29 -0400

    committed IACPipeline - 03-14-2021-2

--- iac_pipeline.groovy
+++ iac_pipeline.groovy
@@ -6,7 +6,7 @@ pipeline {
       steps {
           catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
             withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'aliTestJenkinsUser02', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-              sh "aws cloudformation deploy --template-file $workspace/cloudformation/TrainingEvent-UbuntuServer.json --stack-name TomCatWeb-Stack-Test --region us-east-1 --parameter-overrides InstanceType=t2.micro KeyName=myTestKeyPair02 SSHLocation=0.0.0.0/0 --tags name=TomCatWeb-Stack-Test"
+              sh "aws cloudformation deploy --template-file $workspace/cloudformation/TrainingEvent-UbuntuServer.json --stack-name TomCatWeb-Stack-Test --region us-east-1 --parameter-overrides InstanceType=t2.micro KeyName=aliTrainingKeyPair2 SSHLocation=0.0.0.0/0 --tags name=TomCatWeb-Stack-Test"
               sh "echo SKIPPING INFRASTRUCTURE CREATION/UPDATE for now .."
             }//end withCredentials
             sh "exit 0"
@@ -16,8 +16,8 @@ pipeline {
     stage('Update Inventory'){
       steps{
         catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-              withCredentials([sshUserPrivateKey(credentialsId: '43645869-fe60-4838-ab62-94581a9270d1', keyFileVariable: 'myTestKeyPair02')]) {
-                sh 'ansible-playbook ./ansible/playbooks/update_inventory.yml --user ubuntu --key-file ${myTestKeyPair02}' 
+              withCredentials([sshUserPrivateKey(credentialsId: '43645869-fe60-4838-ab62-94581a9270d1', keyFileVariable: 'aliTrainingKeyPair2')]) {
+                sh 'ansible-playbook ./ansible/playbooks/update_inventory.yml --user ubuntu --key-file ${aliTrainingKeyPair2}' 
            }//end withCredentials
           sh "exit 0"
          }//end catchError
@@ -26,8 +26,8 @@ pipeline {
     stage('Configure Tomcat') {
       steps {
         catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-          withCredentials([sshUserPrivateKey(credentialsId: '43645869-fe60-4838-ab62-94581a9270d1', keyFileVariable: 'myTestKeyPair02')]) {
-            sh 'ansible-playbook ./ansible/playbooks/tomcat-setup.yml --user ubuntu -vvv --key-file ${myTestKeyPair02}'
+          withCredentials([sshUserPrivateKey(credentialsId: '43645869-fe60-4838-ab62-94581a9270d1', keyFileVariable: 'aliTrainingKeyPair2')]) {
+            sh 'ansible-playbook ./ansible/playbooks/tomcat-setup.yml --user ubuntu -vvv --key-file ${aliTrainingKeyPair2}'
             }//end withCredentials
           sh "exit 0"
          }//end catchError
